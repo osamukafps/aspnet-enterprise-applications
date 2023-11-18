b5b1333ddec78196a0a66a47efe26085cd8d8fc3
osamukafps <samuelhplt@gmail.com>
2023-11-18 16:31:28 -0300

    inicio da implementação de autenticação e autorização de usuários

---
+++ NSE.Identidade.API/Data/ApplicationDbContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
+namespace NSE.Identidade.API.Data
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+			: base(options)
+		{
+		}
+
+
+		public static void TestConnection(string connectionString)
+		{
+			using (var connection = new SqlConnection(connectionString))
+			{
+				try
+				{
+					connection.Open();
+					Console.WriteLine($"Conexão aberta com sucesso!" +
+					              $"Database: {connection.Database} connected!");
+					connection.Close();
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine($"Erro ao abrir a conexão: {ex.Message}");
+				}
+			}
+		}
+	}
+}
+
--- NSE.Identidade.API/Program.cs
+++ NSE.Identidade.API/Program.cs
@@ -1,6 +1,20 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using NSE.Identidade.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+ApplicationDbContext.TestConnection(builder.Configuration.GetConnectionString(name:"DefaultConnection"));
+
+builder.Services.AddDefaultIdentity<IdentityUser>()
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -17,6 +31,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
