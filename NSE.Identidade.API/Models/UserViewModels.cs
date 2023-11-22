643803f06bf8deb3befd8bdb48a3b4e9f3062e79
osamukafps <samuelhplt@gmail.com>
2023-11-22 09:37:33 -0300

    implementação inicial da emissão do token pela controller

--- NSE.Identidade.API/Models/UserViewModels.cs
+++ NSE.Identidade.API/Models/UserViewModels.cs
@@ -26,3 +26,23 @@ public class UsuarioLogin
     [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 8)]
     public string Senha { get; set; }
 }
+
+public class UsuarioRespostaLogin 
+{
+    public string AccessToken { get; set; }
+    public double ExpiresIn { get; set; }
+    public UsuarioToken UsuarioToken { get; set; }
+}
+
+public class UsuarioToken
+{
+    public string Id { get; set; }
+    public string Email { get; set; }
+    public IEnumerable<UsuarioClaim> Claims { get; set; }
+}
+
+public class UsuarioClaim
+{
+    public string Value { get; set; }
+    public string Type { get; set; }
+}
